cafe3d14e1cbe158e9f34d68b89aa348f2b3cacd
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-01-27 13:57:09 +0700

    first commit

---
+++ precache-manifest.3b26be73076b03805a8ee2eaef463bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59977639a4bd01035ed951fb682fb313",
+    "url": "/test-ggg/index.html"
+  },
+  {
+    "revision": "6bd5a28ba91061a2238d",
+    "url": "/test-ggg/static/css/main.c84afb88.chunk.css"
+  },
+  {
+    "revision": "41162302271f2c1bbabc",
+    "url": "/test-ggg/static/js/2.3481acfb.chunk.js"
+  },
+  {
+    "revision": "edceaf54630fb3b5c9b66af42a43bff8",
+    "url": "/test-ggg/static/js/2.3481acfb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bd5a28ba91061a2238d",
+    "url": "/test-ggg/static/js/main.5aee084f.chunk.js"
+  },
+  {
+    "revision": "fa29643e3abe04b82e45",
+    "url": "/test-ggg/static/js/runtime-main.f112d213.js"
+  }
+]);
